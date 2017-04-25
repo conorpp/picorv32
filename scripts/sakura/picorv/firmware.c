d8c512366a5ca26d7a15ad2b0a141c8f6edc1450
Conor Patrick <conorpp@vt.edu>
2017-04-25 17:39:57 -0400

    the uart works

--- scripts/sakura/picorv/firmware.c
+++ scripts/sakura/picorv/firmware.c
@@ -1,45 +1,22 @@
-void putc(char c)
+
+// world's most simple boot loader
+
+
+
+void putc(unsigned char c)
 {
-	/**(volatile char*)0x20000000 = c;*/
-	*(volatile char*)0x10000000 = c;
+    *(volatile char*)0x20000000 = c;
 }
 
-void puts(const char *s)
+unsigned char getc()
 {
-	while (*s) putc(*s++);
-}
-
-void *memcpy(void *dest, const void *src, int n)
-{
-	while (n) {
-		n--;
-		((char*)dest)[n] = ((char*)src)[n];
-	}
-	return dest;
+    return *(volatile char*)0x20000000;
 }
 
 void main()
 {
-	char message[] = "$Uryyb+Jbeyq!+Vs+lbh+pna+ernq+guvf+zrffntr+gura$gur+CvpbEI32+PCH"
-			"+frrzf+gb+or+jbexvat+whfg+svar.$$++++++++++++++++GRFG+CNFFRQ!$$";
-	for (int i = 0; message[i]; i++)
-		switch (message[i])
-		{
-		case 'a' ... 'm':
-		case 'A' ... 'M':
-			message[i] += 13;
-			break;
-		case 'n' ... 'z':
-		case 'N' ... 'Z':
-			message[i] -= 13;
-			break;
-		case '$':
-			message[i] = '\n';
-			break;
-		case '+':
-			message[i] = ' ';
-			break;
-		}
-	puts(message);
-	while(1)putc('B');
+    while(1)
+    {
+        putc(getc() + 1);
+    }
 }
--- scripts/sakura/picorv/system.v
+++ scripts/sakura/picorv/system.v
@@ -26,8 +26,8 @@ module system (
 	reg [31:0] mem_rdata;
 
     //wire [7:0] outb = (M_PUSHSW[0] == 1) ? (memory[mem_addr >> 2][7:0]) : out_byte;
-    //assign M_LED = {memory[mem_addr >> 2][1:0], out_byte};
-    assign M_LED = {1'b0, resetn, out_byte};
+    assign M_LED = {memory[mem_addr >> 2][1:0], out_byte};
+    //assign M_LED = {1'b0, resetn, out_byte};
 
     picorv32 
     #(
@@ -45,80 +45,40 @@ module system (
         .mem_rdata   (mem_rdata   )
     );
 
-    //reg [7:0] uart_tx_axi_tdata;
-    //reg uart_tx_axi_tvalid;
-    //wire uart_tx_axi_tready;
+    reg [7:0] uart_tx_axi_tdata;
+    reg uart_tx_axi_tvalid;
+    wire uart_tx_axi_tready;
 
-    //wire [7:0] uart_rx_axi_tdata;
-    //wire uart_rx_axi_tvalid;
-    //reg uart_rx_axi_tready;
+    wire [7:0] uart_rx_axi_tdata;
+    wire uart_rx_axi_tvalid;
+    reg uart_rx_axi_tready;
 
-    //uart u(.clk(clk), .rst(~resetn),
-        //// axi input
-        //.input_axis_tdata(uart_tx_axi_tdata),
-        //.input_axis_tvalid(uart_tx_axi_tvalid),
-        //.input_axis_tready(uart_tx_axi_tready),
-        //// axi output
-        //.output_axis_tdata(uart_rx_axi_tdata),
-        //.output_axis_tvalid(uart_rx_axi_tvalid),
-        //.output_axis_tready(uart_rx_axi_tready),
-        //// uart
-        //.rxd(uart_rxd),
-        //.txd(uart_txd),
-        //// status
-        //.tx_busy(),
-        //.rx_busy(),
-        //.rx_overrun_error(),
-        //.rx_frame_error(),
-        //// configuration
-        //.prescale(52) // 115200 baud (calculate by clkf/(baud*8) = 48000000/(115200*8) )
-    //);
+    uart u(.clk(clk), .rst(~resetn),
+        // axi input
+        .input_axis_tdata(uart_tx_axi_tdata),
+        .input_axis_tvalid(uart_tx_axi_tvalid),
+        .input_axis_tready(uart_tx_axi_tready),
+        // axi output
+        .output_axis_tdata(uart_rx_axi_tdata),
+        .output_axis_tvalid(uart_rx_axi_tvalid),
+        .output_axis_tready(uart_rx_axi_tready),
+        // uart
+        .rxd(uart_rxd),
+        .txd(uart_txd),
+        // status
+        .tx_busy(),
+        .rx_busy(),
+        .rx_overrun_error(),
+        .rx_frame_error(),
+        // configuration
+        .prescale(52) // 115200 baud (calculate by clkf/(baud*8) = 48000000/(115200*8) )
+    );
 
-    //reg [7:0] rx_data;
-    //reg [7:0] tx_data;
+    reg [31:0] memory [0:MEM_SIZE-1];
+    initial $readmemh("firmware.hex", memory);
 
-    //always@(posedge clk)
-    //begin
-        //if (~resetn)
-        //begin
-            //rx_data <= 0;
-            //tx_data <= 0;
-            //uart_tx_axi_tvalid <= 0;
-            //uart_tx_axi_tdata <= 0;
-        //end
-        //else
-        //begin
-            //rx_data <= rx_data;
-            //tx_data <= tx_data;
-
-            //if (uart_rx_axi_tvalid)
-            //begin
-                //rx_data <= uart_rx_axi_tdata;
-            //end
-
-            //if (uart_tx_axi_tready)
-            //begin
-                //uart_tx_axi_tvalid <= 1 & (~M_PUSHSW[0]);
-                //uart_tx_axi_tdata <= 8'h41;
-            //end
-            //else
-            //begin
-                //uart_tx_axi_tdata <= 0;
-                //uart_tx_axi_tvalid <= 0;
-            //end
-        //end
-    //end
-
-    //always@*
-    //begin
-        //uart_rx_axi_tready = 1;
-    //end
-
-	reg [31:0] memory [0:MEM_SIZE-1];
-	initial $readmemh("firmware.hex", memory);
-
-	reg [31:0] m_read_data;
-	reg m_read_en;
+    reg [31:0] m_read_data;
+    reg m_read_en;
 
     always @(posedge clk) begin
         m_read_en <= 0;
@@ -130,8 +90,8 @@ module system (
         out_byte_en <= 0;
         out_byte <= out_byte;
 
-        //uart_rx_axi_tready <= 0;
-        //uart_tx_axi_tvalid <= 0;
+        uart_rx_axi_tready <= 0;
+        uart_tx_axi_tvalid <= 0;
 
 
         if (mem_valid && !mem_ready && !mem_wstrb && (mem_addr >> 2) < MEM_SIZE) 
@@ -152,23 +112,38 @@ module system (
             out_byte <= mem_wdata;
             mem_ready <= 1;
         end
-        //if (mem_valid && !mem_ready && mem_addr == 32'h2000_0000)
-        //begin
-            //if (|mem_wstrb)
-            //begin
-                //// write
-                //mem_ready <= 1;
-                //uart_tx_axi_tdata <= mem_wdata;
-                //uart_tx_axi_tvalid <= 1;
-            //end
-            //else
-            //begin
-                ////read
-                //mem_ready <= uart_rx_axi_tvalid;
-                //mem_rdata <= uart_rx_axi_tdata;
-                //uart_rx_axi_tready <= 1;
-            //end
-        //end
+        if (mem_valid && !mem_ready && mem_addr == 32'h2000_0000)
+        begin
+            if (|mem_wstrb)
+            begin
+                // write to TX
+                mem_ready <= 1;
+                uart_tx_axi_tdata <= mem_wdata;
+                uart_tx_axi_tvalid <= 1;
+            end
+            else
+            begin
+                //read RX data
+                mem_ready <= uart_rx_axi_tvalid;
+                mem_rdata <= uart_rx_axi_tdata;
+                uart_rx_axi_tready <= 1;
+            end
+        end
+
+        if (mem_valid && !mem_ready && mem_addr == 32'h2000_0040)
+        begin
+                //read TX ready signal
+                mem_ready <= 1;
+                mem_rdata <= uart_rx_axi_tvalid;
+        end
+
+
+        if (mem_valid && !mem_ready && mem_addr == 32'h2000_0080)
+        begin
+                //read TX ready signal
+                mem_ready <= 1;
+                mem_rdata <= uart_tx_axi_tready;
+        end
 
     end
 
